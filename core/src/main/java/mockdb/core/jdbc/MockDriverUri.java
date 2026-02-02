9a5be862f80ce59ada0a07e38accf38f1baa7020
zhouchao03 <zhouchao03@inspur.com>
2026-02-02 18:04:03 +0800

    feat: try complete MockDriverUri.

--- core/src/main/java/mockdb/core/jdbc/MockDriverUri.java
+++ core/src/main/java/mockdb/core/jdbc/MockDriverUri.java
@@ -1,10 +1,33 @@
 package mockdb.core.jdbc;
 
-public final class MockDriverUri {
-    private static final String JDBC_URL_PREFIX = "mockdb:";
+import mockdb.core.exceptions.MockSQLException;
+import org.apache.commons.lang3.StringUtils;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.SQLException;
+
+public final class MockDriverUri {
+    private static final String JDBC_URL_PREFIX = "jdbc:";
+    private static final String JDBC_URL_START = JDBC_URL_PREFIX + "mockdb:";
 
     public static boolean acceptsURL(String url) {
-        return url.startsWith(JDBC_URL_PREFIX);
+        return url.startsWith(JDBC_URL_START);
+    }
+
+    private static URI parseDriverUrl(String url) throws SQLException {
+        if (!url.startsWith(JDBC_URL_START)) throw new MockSQLException("Invalid JDBC URL: %s", url);
+        if (url.equals(JDBC_URL_START)) throw new MockSQLException("Empty JDBC URL: %s", url);
+        URI uri;
+        try {
+            uri = new URI(url.substring(JDBC_URL_PREFIX.length()));
+        } catch (URISyntaxException e) {
+            throw new MockSQLException(e, "Invalid JDBC URL: %s", url);
+        }
+
+        if (StringUtils.isBlank(uri.getHost())) throw new MockSQLException("No host specified: %s", url);
+        if (uri.getPort() == -1) throw new MockSQLException("No port number specified: %s", url);
+        if ((uri.getPort() < 1) || (uri.getPort() > 65535)) throw new MockSQLException("Invalid port number: %s", url);
+        return uri;
     }
 }
